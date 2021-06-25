1e73214ba789dc0d208f5d9170c1b54da47f0cfc
Amanoj3 <amanoj3@uic.edu>
2021-06-25 06:56:49 -0500

    bare bones project as of now

--- Main.java
+++ Main.java
@@ -3,8 +3,16 @@ import java.awt.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void setUp() {
+        JFrame window = new JFrame();
+        window.setSize(500,500);
+        window.setLayout(null);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
 
+    public static void main(String[] args){
+        setUp();
         System.out.println("foo");
 
     }
